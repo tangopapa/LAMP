73f18459477d7c6e3963a950ae68218d0614fb60
tangopapa <tom@dtool.com>
2018-03-26 15:41:44 -0400

    changed the way services start

--- start-services.sh
+++ start-services.sh
@@ -10,7 +10,7 @@ try() { "$@" || die "cannot $*"; }
 #!/bin/bash
 
 # Start the first process
-service mysql start -D
+/usr/bin/mysqld_safe -DDFOREGROUND
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start MariaDB service: $status"
@@ -18,11 +18,25 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start the second process
-service apache2 start -D
+/usr/sbin/apachectl -DFOREGROUND -k start > /dev/null 2>&1
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start Apache2 service: $status"
   exit $status
 fi
+
+# Naive check runs checks once a minute to see if either of the processes exited.
+
+while sleep 60; do
+  ps aux |grep my_first_process |grep -q -v grep
+  PROCESS_1_STATUS=$?
+  ps aux |grep my_second_process |grep -q -v grep
+  PROCESS_2_STATUS=$?
+  # If the greps above find anything, they exit with 0 status
+  # If they are not both 0, then something is wrong
+  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+    echo "One of the processes has already exited."
+    exit 1
+  fi
 
 exec "$@"
