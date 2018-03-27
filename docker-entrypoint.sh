0c6ff1b3289bfbd7b9cb6a1612fddd13d6bcc6e7
tangopapa <tom@dtool.com>
2018-03-27 18:56:06 -0400

    modified docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eo pipefail
 
+if [ -z "$1" ]
+then
+  exec /usr/sbin/apachectl -DFOREGROUND -k start > /dev/null 2>&1
+else
+  exec "$@"
+fi
+
 mysqld_safe --bind-address=0.0.0.0
 
 exec "$@"
