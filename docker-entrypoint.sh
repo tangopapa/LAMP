d073ffc739cbb53257e0b58460624ec933921e70
tangopapa <tom@dtool.com>
2018-03-27 17:24:57 -0400

    added certs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,9 +8,10 @@ die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
 
-# Start Apache
+## Start Apache
 /usr/sbin/apachectl -DFOREGROUND -k start &
 status=$?
+## apachectl script returns a 0 exit value on success, and >0 if an error occurs
 if [ $status -ne 0 ]; then
   echo "Failed to start Apache2 service: $status"
   exit $status
