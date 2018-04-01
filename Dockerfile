cf773b66d585b082ece122df6c87909e5be7ce34
tangopapa <tom@dtool.com>
2018-04-01 13:22:45 -0400

    reset to #318

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,10 @@ LABEL maintainer="tom@frogtownroad.com"
 RUN apt-get update  -y
 
 ## Install supplementary packages --no-install-recommends
-RUN apt-get install wget supervisor openssh-server ca-certificates gcc binutils make  -y  
+RUN apt-get install wget supervisor openssh-server ca-certificates --no-install-recommends -y  
 
 ## Install Apache
-RUN apt-get install apache2 libapache2-mod-php7.0  -y
+RUN apt-get install apache2 libapache2-mod-php7.0  --no-install-recommends -y
 
 ## Install PHP
 RUN apt-get install php7.0 php7.0-mysql   --no-install-recommends -y
@@ -31,7 +31,7 @@ apt-get install mariadb-client mariadb-server  --no-install-recommends -y
 RUN mysqld_safe & until mysqladmin ping >/dev/null 2>&1; do sleep 1; done               && \
     mysql -uroot -e "DROP USER IF EXISTS wp_user;"                                      && \
 ## Let's add a root user with no password
-##    mysql -uroot -e "CREATE USER 'root' IDENTIFIED BY '';"                              && \
+    mysql -uroot -e "CREATE USER 'root' IDENTIFIED BY '';"                              && \
     mysql -uroot -e "CREATE USER 'wp_user' IDENTIFIED BY 'wp_password';"                && \
     mysql -uroot -e "DROP DATABASE IF EXISTS wp_database;"                              && \
     mysql -uroot -e "CREATE DATABASE wp_database;"                                      && \
@@ -39,17 +39,18 @@ RUN mysqld_safe & until mysqladmin ping >/dev/null 2>&1; do sleep 1; done       
     mysql -uroot -e "FLUSH PRIVILEGES;"                                                 
 
 
-## Moved to here in Dockerfile so that MariaDB & WP would not have to keep being rebuilt
-## Configure apache2: This script generates a cert for https
-ADD ./start.sh /start.sh
-RUN chmod 755 /start.sh && ./start.sh
-
 ## Install Wordpress - this times out sometimes. Just restart. Script is idempotent
 RUN wget https://wordpress.org/latest.tar.gz    && \
 tar xpf latest.tar.gz                           && \
 rm -rf latest.tar.gz                            && \
 rm -rf /var/www/html                            && \
 cp -r wordpress /var/www/html                   
+
+
+## Moved to here in Dockerfile so that MariaDB & WP would not have to keep being rebuilt
+## Configure apache2: This script generates a cert for https
+ADD ./start.sh /start.sh
+RUN chmod 755 /start.sh
 
 ## Add supervisord.conf to startup the 3 executables - ssh, apache2. mysqld
 COPY ./supervisord.conf /etc/supervisor/conf.d/supervisord.conf
@@ -64,9 +65,4 @@ find /var/www/html -type f -exec chmod 777 {} \;
 ## ENTRYPOINT ["docker-entrypoint.sh"]
 EXPOSE 22 80 443 3306
 
-CMD ["exec /usr/bin/supervisord -n"]
-
-
-
-
-
+CMD ["/bin/bash", "/start.sh"]
--- start.sh
+++ start.sh
@@ -9,22 +9,32 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
-## Create directories sshd requires
-mkdir -p /run/sshd
+## Let's replace current version of openssl w/ heartbleed vulnerable version: openssl-1.0.1d
+## It ends up in /usr/local/ssl/bin/
+mkdir -p /opt
+cd /opt
+wget https://www.openssl.org/source/old/1.0.1/$OPENSSL.tar.gz
 
-## Install openssl  -- move this to dockerfile if this was the issue
-apt-get install openssl -y
+if [ ! -f /opt/$OPENSSL.tar.gz ]; then
+    wget https://www.openssl.org/source/old/1.0.1/$OPENSSL.tar.gz
+fi
+
+tar -xvzf $OPENSSL.tar.gz
+cd $OPENSSL
+./config --prefix=/usr
+make 
+make install
 
 make_cert () {
- openssl req \
--new \
--newkey rsa:4096 \
--days 365 \
--nodes \
--x509 \
--subj "/C=US/ST=VA/L=Upperville/O=dtool/CN=www.$SITE.com" \
--keyout www.$SITE.com.key \
--out www.$SITE.com.cert
+/usr/local/ssl/bin/openssl req \
+                          -new \
+                          -newkey rsa:4096 \
+                          -days 365 \
+                          -nodes \
+                          -x509 \
+                          -subj "/C=US/ST=VA/L=Upperville/O=dtool/CN=www.$SITE.com" \
+                          -keyout www.$SITE.com.key \
+                          -out www.$SITE.com.cert
 }
 
 ## Enable SSL module
@@ -41,14 +51,6 @@ fi
 ## Apache has an SSL template; let's use that
 a2ensite default-ssl
 
-## Modify default-ssl.conf
-sed -i.bak "/^\s*ServerAdmin\s*webmaster@localhost/a\ServerName ${SITE}.com:443" /etc/apache2/sites-enabled/default-ssl.conf
-sed -i.bak "/^\s*SSLCertificateFile/c\SSLCertificateFile /etc/apache2/ssl/www.$SITE.com.cert"  /etc/apache2/sites-enabled/default-ssl.conf
-sed -i.bak "/^\s*SSLCertificateKeyFile/c\SSLCertificateKeyFile /etc/apache2/ssl/www.$SITE.com.key"  /etc/apache2/sites-enabled/default-ssl.conf
-rm -rf /etc/apache2/sites-enabled/*bak
-echo /etc/apache2/sites-enabled/default-ssl.conf
-
-
 ## Restart Apache to effect these changes
 ## Check to see if apache2 has started; if not, start it
 if [[ -z $(pgrep apache2) ]]; then 
@@ -63,18 +65,18 @@ cd /etc/apache2/ssl
 make_cert
 chmod 600 /etc/apache2/ssl/*
 
-
-## Error checking
-cd /
-z=$(find . -print | grep  'bin/openssl')
-echo "Where is openssl?"
-echo -e $z
-echo "***********************************************************************************************************************"
-echo /etc/apache2/sites-enabled/default-ssl.conf
+## Create directories sshd requires
+mkdir -p /run/sshd
 
 ## Modify sshd.config banner greeting
 ## sed -i.bak "/^#Banner none/c\Banner *** WELCOME TO DOCKTER-TOM ***" /etc/ssh/sshd.config
 ## rm -rf *bak
+
+## Modify default-ssl.conf
+sed -i.bak "/^\s*ServerAdmin\s*webmaster@localhost/a\ServerName ${SITE}.com:443" /etc/apache2/sites-enabled/default-ssl.conf
+sed -i.bak "/^\s*SSLCertificateFile/c\SSLCertificateFile /etc/apache2/ssl/www.$SITE.com.cert"  /etc/apache2/sites-enabled/default-ssl.conf
+sed -i.bak "/^\s*SSLCertificateKeyFile/c\SSLCertificateKeyFile /etc/apache2/ssl/www.$SITE.com.key"  /etc/apache2/sites-enabled/default-ssl.conf
+rm -rf /etc/apache2/sites-enabled/*bak
 
 ## This is a soft link
 if [ -e /etc/apache2/sites-available/default-ssl.conf ]; then
@@ -86,6 +88,8 @@ ln -s /etc/apache2/sites-enabled/default-ssl.conf /etc/apache2/sites-available/d
 mkdir -p /var/log/supervisor
 touch /var/log/supervisor/supervisord.log
 
-## exec /usr/bin/supervisord -n                                        
+exec /usr/bin/supervisord -n                                        ##-c /etc/supervisor/conf.d/supervisord.conf
+echo "starting supervisor..."
+
 ## while true; do sleep 1; done
-## exec "$@"
+exec "$@"
